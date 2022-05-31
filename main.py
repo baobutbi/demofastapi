97a6577a3af0c8bd9d50a49957332380d58fcccb
baobutbi <106371649+baobutbi@users.noreply.github.com>
2022-05-31 14:32:45 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+from . import crud, models, schemas
+from .database import engine, SessionLocal
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello"}
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
+@app.get("/car/", response_model=list[schemas.Car])
+def read_car(db: Session = Depends(get_db), skip: int = 0, limit: int = 10):
+    cars = crud.get_car(db=db, skip=skip, limit=limit)
+    return cars
+
+
+@app.post("/create", response_model=schemas.Car)
+def create_car(car: schemas.CarModel, db: Session = Depends(get_db)):
+    return crud.add_car(db=db, car=car)
+# 123345678
